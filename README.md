907c4967855a0cfbc0a618dcfb6bc23889ed6ba9
kousuke <kousuke.econ15060709@hotmail.co.jp>
2016-09-11 23:34:04 +0900

    readme 修正

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# crawler
+# careerticket newsサービス
+
+## ct用の就活情報メールサービスです。
+
+
+## web周りはsitnatraでやる予定
--- app.rb
+++ app.rb
@@ -3,34 +3,82 @@ require 'capybara/poltergeist'
 
 
 
-get '/' do
-  'Hello world!'
+# get '/' do
+#   puts "hello world"
+
+# 	erb :index
+# end
+
+
+# post 'save/user' do
+#   puts "hello world"
+# end
+
+
+module Crawler
+  def fetch_html
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app, {:js_errors => false, :timeout => 1000 })
+    end
+    session = Capybara::Session.new(:poltergeist)
+    session.visit "http://www.b-ch.com/ttl/index.php?ttl_c=2277"
+    puts session.status_code
+    puts '各話タイトル'
+
+    # 「各話あらすじ」をクリックする => onClickが実行される 
+    session.find('div.ttlinfo-menu').all('ul')[0].all('li')[2].find('a').click
+
+    # 第１話タイトル
+    puts session.find('div#ttlinfo-stry').find('dt').text
+
+    # 第２話タイトル〜
+    # 最終話は動的に取得してもよいかも
+    2.upto(44) do |num|
+      # onClickイベント　ページネーションクリック
+      session.find('div#ttlinfo-stry').find('p#page-list').click_link num.to_s
+      sleep 3 # ajaxで内容が書き換えられる間少し待つ。待ち時間は適当...
+      puts session.find('div#ttlinfo-stry').find('dt').text
+    end
+
+    return 'url','title'
+  end
+
+  def parse_content(html)
+    
+    return content
+  end
 end
 
 
-post 'save' do
-  
+
+# dbへ保存する系
+class Orm < active_record
+  # ハッシュを入れる
+  def save()
+    
+  end
 end
 
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {:js_errors => false, :timeout => 1000 })
+
+
+class Nikkei
+  def initialize(args)
+    
+  end
+
+  def login
+    
+  end
 end
-session = Capybara::Session.new(:poltergeist)
-session.visit "http://www.b-ch.com/ttl/index.php?ttl_c=2277"
-puts session.status_code
-puts '各話タイトル'
 
-# 「各話あらすじ」をクリックする => onClickが実行される 
-session.find('div.ttlinfo-menu').all('ul')[0].all('li')[2].find('a').click
 
-# 第１話タイトル
-puts session.find('div#ttlinfo-stry').find('dt').text
+class NewsPicks
+  def initialize(args)
+    login(username, password)
+  end
 
-# 第２話タイトル〜
-# 最終話は動的に取得してもよいかも
-2.upto(44) do |num|
-  # onClickイベント　ページネーションクリック
-  session.find('div#ttlinfo-stry').find('p#page-list').click_link num.to_s
-  sleep 3 # ajaxで内容が書き換えられる間少し待つ。待ち時間は適当...
-  puts session.find('div#ttlinfo-stry').find('dt').text
+  def login(username, password)
+    
+  end
 end
+
