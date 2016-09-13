6b4e06b9954ac4c5b65e60663c8220213c1c8f46
kousuketanihata <kousuke.econ15060709@hotmail.co.jp>
2016-09-13 22:51:06 +0900

    done

---
+++ main.sql
@@ -0,0 +1,17 @@
+create database mail_news;
+create table mail_news.user(
+    id int not null auto_increment primary key,
+    name text,
+    email text,
+    create_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+create table mail_news.content(
+    id int not null auto_increment primary key,
+    url text UNIQUE,
+    title text,
+    html text,
+    content text,
+    create_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
